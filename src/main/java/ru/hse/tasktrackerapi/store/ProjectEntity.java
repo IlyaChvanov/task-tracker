c4f5d7ae2b51121651190d3406ad7e4c7ac71158
ilya <ivchvanov@edu.hse.ru>
2025-01-05 01:06:59 +0300

    feat(persistence): add project, task, taskState entities

---
+++ src/main/java/ru/hse/tasktrackerapi/store/ProjectEntity.java
@@ -0,0 +1,31 @@
+package ru.hse.tasktrackerapi.store;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "project")
+public class ProjectEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(unique = true)
+    private String name;
+
+    private Instant creationDate = Instant.now();
+
+    @OneToMany
+    private List<TaskStateEntity> taskStates = new ArrayList<>();
+}
---
+++ src/main/java/ru/hse/tasktrackerapi/store/TaskStateEntity.java
@@ -0,0 +1,30 @@
+package ru.hse.tasktrackerapi.store;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "task_state")
+public class TaskStateEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(unique = true)
+    private String name;
+
+    private Long ordinal;
+
+    @OneToMany
+    private List<TaskEntity> tasks = new ArrayList<>();
+}
